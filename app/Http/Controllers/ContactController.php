359486f54444b5aabc36e84357f471eb12fc2cf1
Alessio Allegrini <90352635+AleAlle62@users.noreply.github.com>
2023-05-29 18:38:20 +0200

    db ok

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -1,5 +1,5 @@
 <?php
-use Illuminate\Support\Facades\Mail;
+
 namespace App\Http\Controllers;
 
 use App\Models\Contact;
@@ -8,33 +8,26 @@ use Illuminate\Http\Request;
 class ContactController extends Controller
 {
     public function store(Request $request)
-{
+    {
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'surname' => 'required',
+            'message' => 'required',
+            'photo' => 'image|max:2048',
+        ]);
 
-    $validatedData = $request->validate([
-        'name' => 'required',
-        'surname' => 'required',
-        'message' => 'required',
-        'photo' => 'image|max:2048',
-    ]);
+        $contact = new Contact();
+        $contact->name = $request->name;
+        $contact->surname = $request->surname;
+        $contact->message = $request->message;
 
+        if ($request->hasFile('photo')) {
+            $photoPath = $request->file('photo')->store('photos', 'public');
+            $contact->photo = $photoPath;
+        }
 
-    $contact = new Contact();
-    $contact->name = $request->name;
-    $contact->surname = $request->surname;
-    $contact->message = $request->message;
+        $contact->save();
 
-    if ($request->hasFile('photo')) {
-        $photoPath = $request->file('photo')->store('photos', 'public');
-        $contact->photo = $photoPath;
-    }
-
-    $contact->save();
-
-
-    Mail::send('emails.contact', ['contact' => $contact], function ($message) {
-        $message->to('allegrini.alessio00@gmail.com', 'Your Name')
-                ->subject('New Contact Message');
-    });
-
+        return redirect()->back()->with('success', 'Messaggio inviato con successo!');
     }
 }
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -513,6 +513,7 @@ class ComposerStaticInitaeaffb01755ede9401fd881ccea8876d
         'App\\Http\\Middleware\\TrustHosts' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustHosts.php',
         'App\\Http\\Middleware\\TrustProxies' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustProxies.php',
         'App\\Http\\Middleware\\VerifyCsrfToken' => __DIR__ . '/../..' . '/app/Http/Middleware/VerifyCsrfToken.php',
+        'App\\Models\\Contact' => __DIR__ . '/../..' . '/app/Models/Contact.php',
         'App\\Models\\User' => __DIR__ . '/../..' . '/app/Models/User.php',
         'App\\Providers\\AppServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AppServiceProvider.php',
         'App\\Providers\\AuthServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AuthServiceProvider.php',
