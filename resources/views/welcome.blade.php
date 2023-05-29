21eb385e78466279748fdc5077e367455d1755c6
Alessio Allegrini <90352635+AleAlle62@users.noreply.github.com>
2023-05-29 18:55:58 +0200

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- fontawesome --}}
+    <script src="https://kit.fontawesome.com/c88a405b1b.js" crossorigin="anonymous"></script>
     <title>Progetto Form</title>
 </head>
 <body>
     <form method="POST" action="{{ route('contact.store') }}" enctype="multipart/form-data">
         @csrf
         <div class="form-group">
-            <label for="name">Nome</label>
+                <label for="name">Nome</label>
+            </div>
             <input type="text" name="name" id="name" class="form-control" required>
         </div>
 
@@ -31,6 +34,64 @@
 
         <button type="submit" class="btn btn-primary">Invia</button>
     </form>
+
+
+    <style>
+        in {
+            gap: 10px;
+        }
+        body {
+            font-family: Arial, sans-serif;
+        }
+
+        form {
+            max-width: 400px;
+            margin: 0 auto;
+        }
+
+        .form-group {
+            margin-bottom: 20px;
+        }
+
+        label {
+            display: block;
+            font-weight: bold;
+            margin-bottom: 5px;
+        }
+
+        input[type="text"],
+        textarea {
+            width: 100%;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+            resize: vertical;
+        }
+
+        .form-control-file {
+            display: block;
+            padding: 5px;
+        }
+
+        button {
+            width: 100%;
+            background-color: #4CAF50;
+            color: white;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #45a049;
+        }
+
+        .error {
+            color: red;
+        }
+    </style>
 
 </body>
 </html>
